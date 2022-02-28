1ab89b9af878804880a70111242f06d35dc1049f
Sai Chaitanya Reddy <munna131095@gmail.com>
2022-02-28 12:04:20 +0530

    initialcommit made

---
+++ happy.py
@@ -0,0 +1,14 @@
+def sum_sqr(num):
+    x=0
+    for i in range(len(str(num))):
+        y= str(num)
+        x= x+ int(y[i])*int(y[i])
+        print(x)
+    if len(str(x)) !=1:
+       return sum_sqr(x)
+    elif x ==1:
+        return True
+    else:
+        return False
+n = int(input('enter the number:'))
+print(sum_sqr(n))
